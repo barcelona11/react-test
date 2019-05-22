e7406d2d29b5c5c89ccad2be0ee3c9050dbeaa32
kwli <kwli@ctrip.com>
2019-05-22 10:16:29 +0800

    Updates

---
+++ precache-manifest.4d96c4a5172254389b2967b6e2b7fa7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca197977d22c4d3b15be4ce40633a8e3",
+    "url": "/react-test/index.html"
+  },
+  {
+    "revision": "b452ef03d2bc83767115",
+    "url": "/react-test/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/react-test/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "b452ef03d2bc83767115",
+    "url": "/react-test/static/js/main.3a724bd5.chunk.js"
+  },
+  {
+    "revision": "91e87af5d0bfc2a79f8e",
+    "url": "/react-test/static/js/runtime~main.b9ad4e01.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
